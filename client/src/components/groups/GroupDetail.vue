fbce617cbccac797993dd8907579bc03dac080b9
aminov <alami@tshtt.uz>
2021-10-07 21:00:02 +0500

    add GroupViewModel 24:50

--- client/src/components/groups/GroupDetail.vue
+++ client/src/components/groups/GroupDetail.vue
@@ -1,13 +1,13 @@
 <template>
-<p>Detail</p>
+<span>{{ group.name }}</span>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
+
+import { GroupViewModel } from './models'
+
 export default class GroupDetail extends Vue {
+  private group!: GroupViewModel;
 }
 </script>
-
-<style scoped>
-
-</style>
