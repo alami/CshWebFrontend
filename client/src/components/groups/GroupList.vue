8e80c67554a6e2d9a8a8dfdf8048fe0fa911868a
aminov <alami@tshtt.uz>
2021-10-08 11:11:16 +0500

    GroupList 53:0

--- client/src/components/groups/GroupList.vue
+++ client/src/components/groups/GroupList.vue
@@ -1,7 +1,7 @@
 <template>
   <ul>
     <li v-for="group in groups" v-bind:key="group.id">
-      <GroupDetail v-bind:group="group" />
+      <GroupDetail v-bind:group="group"  v-on:update="onUpdate" v-on:remove="onRemove" />
     </li>
   </ul>
 </template>
@@ -18,5 +18,19 @@ import {GroupViewModel} from "@/components/groups/models";
 })
 export default class GroupList extends Vue {
   @Prop() private groups!: GroupViewModel[];
+
+  private onUpdate(group: GroupViewModel): void {
+    console.log(group);
+    this.$emit('update', group);
+  }
+
+  private onRemove(groupId: number): void {
+    console.log(groupId);
+    this.$emit('remove', groupId);
+  }
+
+  private onAdd(group: GroupViewModel): void {
+    this.$emit('add', group);
+  }
 }
 </script>
